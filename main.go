347e66ffdc8e21a427a0dba637d3f404c76d1bdd
Erkebu1an <113977320+Erkebu1an@users.noreply.github.com>
2023-05-26 10:38:26 +0600

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
+// * pointer
+// & adress
+
+func changeLocal(num *[5]int) { // *
+  num[0] = 55
+
+  fmt.Println("\ninside function:\n ", &num)
+
+}
 func main() {
-	sum := 0
-	for i := 0; i < 10; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
+  num := [...]int{5, 6, 7, 8, 8}
+  fmt.Println("\nbefore passing to function:\n ", &num)
+
+  changeLocal(&num) //num is passed by value
+  fmt.Println("\nadress of num:\n  ", &num)
+
+  fmt.Println("\nafter passing to function: ", &num)
 }
